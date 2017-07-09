89a0a3516d6b4b69136a9544659320f93064e04f
jarnalyrkar <joachimsolstrom@gmail.com>
2017-07-09 22:22:45 +0200

    Another exercise done!

--- K1. 07.02/SumNumbersInFile.java
+++ K1. 07.02/SumNumbersInFile.java
@@ -1,26 +1,47 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class SumNumbersInFile {
-	public static void main(String[] args) {
-		String filename = "thefile.txt";
-		String content = "";
-		int sum = 0;
-		
-		try {
-			content = new String(Files.readAllBytes(Paths.get(filename)));
-			System.out.println(content);
-			Scanner sc = new Scanner(new File("thefile.txt"));
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-			System.out.println(sum);
 
+	public static void main(String[] args) {
+
+		String filename = "thefile.txt";
+	try {
+		Scanner sc = new Scanner(new File(filename));
+		
+		// Count how many lines exists
+		int lines = 0;
+		while (sc.hasNextLine()) {
+			sc.nextLine();
+			lines++;
+		}
+			
+		// Makes an array of size == number of lines in the document.
+		int[] sumArr = new int[lines];
+		String line = "";
+		BufferedReader br = new BufferedReader(new FileReader(filename));
+		
+		for (int i = 0; i < lines; i++) {
+			line = br.readLine();
+			sc = new Scanner(line);
+			while (sc.hasNext()){
+				sumArr[i] += sc.nextInt();
+			}
+		}
+		br.close();
+		sc.close();
+		
+		System.out.println(Arrays.toString(sumArr));		
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
 	}
 
 }
